1e93079189302882f9f59051d6f2be5b09e29d3f
dwehusijok <psjcarkerbabyyy@hotmail.com>
2024-05-13 13:28:49 +0700

    update aptos task

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-function minCostClimbingStairs(cost) {
-  const n = cost.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = 2; i <= n; i++) {
-    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+function solveSudoku(board) {
+  solve(board);
+  function solve(board) {
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (board[i][j] === ".") {
+          for (let num = 1; num <= 9; num++) {
+            const numChar = num.toString();
+            if (isValid(board, i, j, numChar)) {
+              board[i][j] = numChar;
+              if (solve(board)) return true;
+              board[i][j] = ".";
+            }
+          }
+          return false;
+        }
+      }
+    }
+    return true;
   }
-  return dp[n];
+  function isValid(board, row, col, num) {
+    const boxRow = Math.floor(row / 3) * 3;
+    const boxCol = Math.floor(col / 3) * 3;
+    for (let i = 0; i < 9; i++) {
+      if (
+        board[row][i] === num ||
+        board[i][col] === num ||
+        board[boxRow + Math.floor(i / 3)][boxCol + (i % 3)] === num
+      )
+        return false;
+    }
+    return true;
+  }
 }
